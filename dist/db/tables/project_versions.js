1b48982d33c4c5953ff6779e97dba1dd182d1af0
natasha-maryam <142990310+natasha-maryam@users.noreply.github.com>
2025-08-19 16:56:09 +0500

    deleted build file

--- dist/db/tables/project_versions.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// This file contains TypeScript interfaces for project versions table
-// Table creation is handled by Knex migrations
-Object.defineProperty(exports, "__esModule", { value: true });
--- dist/db/tables/users.js
+++
@@ -1,4 +0,0 @@
-"use strict";
-// This file contains TypeScript interfaces for user-related tables
-// Table creation is handled by Knex migrations
-Object.defineProperty(exports, "__esModule", { value: true });
