49d748cd7fc55c27db4e0a0157326f3bed44e166
natasha-maryam <142990310+natasha-maryam@users.noreply.github.com>
2025-08-21 13:49:39 +0500

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -48,7 +48,7 @@ app.use(
     },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization", "X-Requested-With"],
+    allowedHeaders: ["*"],
   })
 );
 
